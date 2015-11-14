bffa0eb9a8a50bea7d866005c06c34fe243f5f76
Brian Guo <Brian@dhcp-hol-2243.eduroam.cornell.edu>
2015-11-14 16:29:58 -0500

    subjects sectionalized

--- routes.js
+++ routes.js
@@ -11,15 +11,14 @@ Router.route('/', {
 // POST ROUTES
 
 // Individual
-Router.route('/:subject', {
-	name: 'academic',
-	template: 'tab',
-	waitOn: function() {
+Router.route('/:subject', function(){
+	var name= 'academic';
+	var template= 'tab';
+	var waitOn = function() {
 		return Meteor.subscribe('categories');
-	},
-	data: function() {
-		 posts: Posts.find({category: this.params.subject});
-	}, 
+	};
+	var posts = Posts.find({category: this.params.subject});
+	this.render('tab', {data: {posts: posts}});
 
 });
 
