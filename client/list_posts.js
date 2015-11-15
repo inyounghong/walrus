a2376df849e39c9c48de331f0fe42ca8ff7a7dc3
Inyoung Hong <ih235@cornell.edu>
2015-11-14 21:56:21 -0500

    delete css

--- client/list_posts.js
+++ client/list_posts.js
@@ -24,23 +24,7 @@ Template.index.events({
     Session.set("hideCompleted", event.target.checked);
   },
 
-  "click #loginWithGoogle": function(err, t){
-    Meteor.loginWithGoogle({
-      requestPermissions: ["email"],
-      loginStyle: "popup"
-    }, function(err) {
-      if (err) {
-        // TODO Need to do something here with the error...
-        console.log('Error: ', err);
-      } 
-    });
-  },
 
-  "click #logout": function (err, t){
-    Meteor.logout(function(err) {
-
-    });
-  }
   
 });
 
