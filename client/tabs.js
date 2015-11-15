25fc2d70ebdbc6fd8cedc10cce0d3a738e776390
Inyoung Hong <ih235@cornell.edu>
2015-11-14 21:30:45 -0500

    form styling

--- client/tabs.js
+++ client/tabs.js
@@ -8,13 +8,16 @@ Template.tab.helpers( {
 			filter = "Top";
 		}
 		if (filter == "Top"){
-			console.log(this.subject);
 			return Posts.find({category: this.subject}, {sort: {votes: -1}});
 		}
 
 		else if (filter == "Oldest") {
 			return Posts.find({category: this.subject}, {sort: {createdAt: 1}});
 		}
+	},
+
+	title: function(){
+		return this.subject;
 	},
 
 	topIsSelected: function() {
