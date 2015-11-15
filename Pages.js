56f02cdca0d8387fd15fc7b20c0aaa13d71de46a
Brian Guo <Brian@dhcp-hol-2243.eduroam.cornell.edu>
2015-11-15 03:41:48 -0500

    added

---
+++ Pages.js
@@ -0,0 +1,17 @@
+
+
+/*Pages = new Meteor.Pagination("posts", {
+	perPage: 5,
+	availableSettings: {
+		perPage: true,
+		sort: true
+	},
+	itemTemplate: "list_post",
+	//homeRoute: '/posts',
+	router: "iron-router",
+	sort:{
+		id: 1
+	},
+	templateName: "index",
+	routerTemplate:"index"
+});*/
